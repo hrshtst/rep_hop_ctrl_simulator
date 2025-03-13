a9f7056ba830eaeed8dc37918eb2c96d6b2fd180
Hiroshi Atsuta <atsuta@ieee.org>
2025-03-13 22:25:07 +0900

    dynmorph: Fix parameter update when aerial phase

--- src/rhc_ctrl_dynmorph.c
+++ src/rhc_ctrl_dynmorph.c
@@ -159,7 +159,7 @@ double _ctrl_dynmorph_calc_phi_ratio_linear(double phi)
 double _ctrl_dynmorph_calc_phi_ratio_piecewise(double phi)
 {
   double s;
-  if( phi < PI_2 ) s = 1.0;
+  if( phi < -PI_2 ) s = 1.0;
   else s = 0.0;
   return s;
 }
